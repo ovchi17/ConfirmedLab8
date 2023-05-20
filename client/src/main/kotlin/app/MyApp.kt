1aeba6c1b555ee8089c7ddb4b1fafa5e4259d27e
OvchinnikovI17 <66004930+OvchinnikovI17@users.noreply.github.com>
2023-05-20 17:18:10 +0300

    Width and Height

--- client/src/main/kotlin/app/MyApp.kt
+++ client/src/main/kotlin/app/MyApp.kt
@@ -1,8 +1,17 @@
 package app
 
+import javafx.scene.Scene
 import view.MyView
 import tornadofx.*
+import tornadofx.FX.Companion.primaryStage
 import view.Login
 
 
-class MyApp: App(MyView::class)
+class MyApp: App(MyView::class){
+    override fun createPrimaryScene(view: UIComponent): Scene {
+        val scene = Scene(view.root)
+        primaryStage.width = 800.0
+        primaryStage.height = 600.0
+        return scene
+    }
+}
