503a4c66be29f28de6fb0cbeaebccad4503682ea
OvchinnikovI17 <66004930+OvchinnikovI17@users.noreply.github.com>
2023-05-27 03:19:03 +0300

    proxyTokenizator

--- client/src/main/kotlin/proxyTokenizator.kt
+++ client/src/main/kotlin/proxyTokenizator.kt
@@ -1,3 +1,20 @@
+import moduleWithResults.ResultModule
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
 class proxyTokenizator {
-    //TODO: sdelat proxy ispolnitel k tokeinizator
+
+    val tokenizator = KoinTkn().returnTokenizator()
+    fun proxyTokenizator(command: String, argument: List<String>, tkn: String): ResultModule{
+        val res = tokenizator.tokenizator(command, argument, tkn)
+        return res
+    }
+
+}
+
+class KoinTkn: KoinComponent {
+    val tokenizator: Tokenizator by inject()
+    fun returnTokenizator(): Tokenizator{
+        return tokenizator
+    }
 }
